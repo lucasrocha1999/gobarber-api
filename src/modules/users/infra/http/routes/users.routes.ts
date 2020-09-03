e93b74bafdba4fa7a572a6b449daa8a34a5d7b42
Lucas <lucasrocha446@gmail.com>
2020-09-03 16:09:53 -0300

    finalizing ddd module

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import multer from 'multer';
-import uploadConfig from '../config/upload';
+import uploadConfig from '@config/upload';
 
-import CreateUserService from '../services/CreateUserService';
-import UpdadeUserAvatarService from '../services/UpdateUserAvatarService';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdadeUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -13,7 +14,8 @@ const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
 
-  const createUser = new CreateUserService();
+  const userRepository = new UsersRepository();
+  const createUser = new CreateUserService(userRepository);
 
   const user = await createUser.execute({
     name,
@@ -25,7 +27,8 @@ usersRouter.post('/', async (request, response) => {
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-  const updateUserAvatar = new UpdadeUserAvatarService();
+  const userRepository = new UsersRepository();
+  const updateUserAvatar = new UpdadeUserAvatarService(userRepository);
 
   const user = await updateUserAvatar.execute({
     user_id: request.user.id,
